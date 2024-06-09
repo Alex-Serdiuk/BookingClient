39c81204c4c2cb23383fd7beec297b0feefb8976
Alex-Serdiuk <jango.d57@gmail.com>
2024-06-09 20:05:02 +0300

    add .env production

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = (url) => {
+const useFetch = (endpoint) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const baseUrl = process.env.REACT_APP_API_URL;
+  const url = `${baseUrl}${endpoint}`;
 
   useEffect(() => {
     const fetchData = async () => {
